51ce8ddf2eda15badf2a964cd184a5696813531f
RohendoJ <rohendoj3134.github@gmail.com>
2024-01-22 15:40:59 +0700

    improve: navbar

--- src/pages/user/login/login.tsx
+++ src/pages/user/login/login.tsx
@@ -5,9 +5,9 @@ import { CardLogin, ImgLogin } from ".";
 export const Login: FC = (): ReactElement => {
   return (
     <main className="relative flex h-[850px] w-full flex-col bg-[url('/backgrounds/green.png')] font-inter">
-      <section className="container h-full w-full">
+      <section className="h-full w-full">
         <Navbar />
-        <section className="flex h-[60%] w-full">
+        <section className="container flex h-[60%] w-full">
           <CardLogin />
           <ImgLogin />
         </section>
