153569aeae46feb6e8e4c95d8d4c99f7a4873e1d
RohendoJ <rohendoj3134.github@gmail.com>
2024-01-31 12:43:41 +0700

    improve: data table

--- src/components/data-table/index.tsx
+++ src/components/data-table/index.tsx
@@ -43,7 +43,7 @@ export function DataTable<TData, TValue>({
   });
 
   return (
-    <div>
+    <div className="w-full">
       <Table>
         <TableHeader className="bg-dark-2">
           {table.getHeaderGroups().map((headerGroup) => (
