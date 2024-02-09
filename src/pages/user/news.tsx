c99175838a9e3cbdfe452561b03b32705d1b2a78
RohendoJ <rohendoj3134.github@gmail.com>
2024-02-09 14:17:21 +0700

    improve: news

--- src/pages/user/news.tsx
+++ src/pages/user/news.tsx
@@ -1,30 +1,36 @@
-import { FC, ReactElement } from "react";
+import { FC, ReactElement, useEffect, useState } from "react";
 import { FaArrowRightLong } from "react-icons/fa6";
-import {
-  Button,
-  Footer,
-  Navbar,
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-  Trend,
-} from "@/components";
+import { Button, Footer, Navbar } from "@/components";
 
 export const News: FC = (): ReactElement => {
-  const labels = [
-    { name: "Tanaman Herbal" },
-    { name: "Teknologi Pengobatan" },
-    { name: "Tips Kesehatan" },
-    { name: "Diet dan Nutrisi" },
-    { name: "Apa Kata Medis" },
-    { name: "Pengobatan Sunah" },
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const trends = [
+    {
+      title: "Si Kuning Kunyit Kaya Manfaat",
+      description:
+        "Kunyit telah dikenal untuk merawat kulit dan membantu menyembuhkan luka..",
+      image: "/images/turmeric.png",
+      label: "Tanaman Herbal",
+      posted: "17 jan 2024",
+    },
+    {
+      title: "Khasiat Kulit Manggis untuk Cegah Kanker, Benarkah?",
+      description: "Masyarakat Indonesia sudah tidak asing..",
+      image: "/images/mangosteen.png",
+      label: "Diet dan Nutrisi",
+      posted: "08 jan 2024",
+    },
+    {
+      title: "Isolat Senyawa Aktif Mannotriose Alternatif Pengobatan Kanker.",
+      description: "Tingkat keberhasilan pengobatan kanker..",
+      image: "/images/mannotriose.png",
+      label: "Teknologi Pengobatan",
+      posted: "02 jan 2024",
+    },
   ];
 
-  const news = [
+  const allNews = [
     {
       title: "Si Kuning Kunyit Kaya Manfaat",
       description:
@@ -48,18 +54,19 @@ export const News: FC = (): ReactElement => {
       posted: "02 jan 2024",
     },
     {
-      title: "Isolat Senyawa Aktif Mannotriose Alternatif Pengobatan Kanker.",
-      description: "Tingkat keberhasilan pengobatan kanker..",
-      image: "/images/mannotriose.png",
-      label: "Teknologi Pengobatan",
-      posted: "02 jan 2024",
+      title: "Si Kuning Kunyit Kaya Manfaat",
+      description:
+        "Kunyit telah dikenal untuk merawat kulit dan membantu menyembuhkan luka..",
+      image: "/images/turmeric.png",
+      label: "Tanaman Herbal",
+      posted: "17 jan 2024",
     },
     {
-      title: "Isolat Senyawa Aktif Mannotriose Alternatif Pengobatan Kanker.",
-      description: "Tingkat keberhasilan pengobatan kanker..",
-      image: "/images/mannotriose.png",
-      label: "Teknologi Pengobatan",
-      posted: "02 jan 2024",
+      title: "Khasiat Kulit Manggis untuk Cegah Kanker, Benarkah?",
+      description: "Masyarakat Indonesia sudah tidak asing..",
+      image: "/images/mangosteen.png",
+      label: "Diet dan Nutrisi",
+      posted: "08 jan 2024",
     },
     {
       title: "Isolat Senyawa Aktif Mannotriose Alternatif Pengobatan Kanker.",
@@ -70,114 +77,175 @@ export const News: FC = (): ReactElement => {
     },
   ];
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.scrollY;
+      setScrollPosition(position);
+    };
+
+    const initialScrollPosition = window.scrollY;
+    setScrollPosition(initialScrollPosition);
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <main className="relative flex h-[3000px] w-full flex-col font-inter">
-      {/* Header */}
-      <Navbar className="bg-white" />
-      <section className="container flex h-[12%] w-full pb-10">
-        <section className="w-1/2">
-          <h1 className="flex h-full w-[55%] items-end text-[2rem] font-bold text-[#1E212B]">
-            Perjalanan edukatif. Mari baca dan pelajari.
-          </h1>
-        </section>
-        <section className="flex h-full w-1/2 flex-col justify-end gap-3">
-          <h2 className="text-xl font-bold text-font-black-1">
-            Ketegori pembahasan.
-          </h2>
-          <p className="w-3/4 text-font-black-2">
-            Mari bergabung dalam perjalanan edukatif ini, dan temukan bagaimana
-            gabungan antara alam dan teknologi dapat mengubah cara kita
-            memandang kesehatan.
-          </p>
-          <section className="flex w-[85%] flex-wrap gap-3">
-            {labels.map((label, index) => (
-              <section
-                key={index}
-                className="grid place-items-center rounded-md border border-bright-2 px-4 py-2 font-bold text-font-black-3"
-              >
-                {label.name}
-              </section>
-            ))}
-          </section>
-        </section>
+    <main className="flex flex-col bg-[url('/backgrounds/white.jpg')]  font-inter">
+      <Navbar
+        className={
+          scrollPosition > 0 ? "bg-white duration-300 ease-in-out" : ""
+        }
+      />
+
+      <section className="mt-32 flex flex-col justify-evenly gap-10 pb-10">
+        <div className="container mt-3 flex flex-row-reverse flex-wrap-reverse justify-between gap-10 md:mt-0 md:flex-wrap md:gap-0">
+          <div className="flex w-full flex-col justify-evenly gap-5 md:w-[45%] md:gap-3">
+            <h1 className="text-2xl font-bold text-font-black-1">
+              Kategori pembahasan.
+            </h1>
+            <p className="text-sm text-font-black-2 xl:text-base">
+              Mari bergabung dalam perjalanan edukatif ini, dan temukan
+              bagaimana gabungan antara alam dan teknologi dapat mengubah cara
+              kita memandang kesehatan.
+            </p>
+            <div className="flex w-full flex-wrap gap-3 font-bold">
+              <div className="grid place-items-center rounded-md border border-bright-2 p-2 text-sm text-font-black-3 xl:text-base">
+                Tanaman Herbal
+              </div>
+              <div className="grid place-items-center rounded-md border border-bright-2 p-2 text-sm text-font-black-3 xl:text-base">
+                Teknologi Pengobatan
+              </div>
+              <div className="grid place-items-center rounded-md border border-bright-2 p-2 text-sm text-font-black-3 xl:text-base">
+                Tips Kesehatan
+              </div>
+              <div className="grid place-items-center rounded-md border border-bright-2 p-2 text-sm text-font-black-3 xl:text-base">
+                Diet dan Nutrisi
+              </div>
+              <div className="grid place-items-center rounded-md border border-bright-2 p-2 text-sm text-font-black-3 xl:text-base">
+                Apa Kata Medis
+              </div>
+              <div className="grid place-items-center rounded-md border border-bright-2 p-2 text-sm text-font-black-3 xl:text-base">
+                Pengobatan Sunah
+              </div>
+            </div>
+          </div>
+          <div className="flex w-full items-end md:w-1/2">
+            <h1 className="w-full text-[2rem] font-bold text-[#1E1E1E] md:text-[2.2rem] xl:w-3/4">
+              Perjalanan edukatif. Mari baca dan pelajari.
+            </h1>
+          </div>
+        </div>
       </section>
 
-      {/* Body */}
-      <Trend />
-      <section className="container flex h-[40%] w-full flex-col bg-white">
-        <hr className="h-[2px] w-full bg-[#36373C]" />
-        <h1 className="mt-7 text-[2rem] font-bold text-font-black-1">
-          All News
+      <section className="flex min-h-[600px] flex-col justify-evenly gap-5 bg-white pt-10 md:gap-0 md:pt-0">
+        <div className="container h-[1px] w-4/5 bg-[#36373C] md:w-[95%]" />
+        <h1 className="container text-[2rem] font-bold text-font-black-1">
+          News Trend
         </h1>
-        <section className="mt-10 flex w-full flex-wrap justify-between gap-10">
-          {news.map((trend, index) => (
-            <section className="flex h-[470px] w-[30%] flex-col" key={index}>
-              <img
-                src={trend.image}
-                alt="trend"
-                className="rounded-md object-cover"
-              />
-              <section className="flex h-[65%] flex-col pt-1">
+        <section className="container flex flex-wrap justify-between gap-10 md:gap-0">
+          {trends.map((trend, index) => (
+            <section
+              className="flex min-h-[400px] w-full flex-col justify-between md:w-[29.5%]"
+              key={index}
+            >
+              <section className="flex flex-col pt-1">
+                <img
+                  src={trend.image}
+                  alt="trend"
+                  className=" rounded-md object-cover"
+                />
                 <section className="flex items-center gap-1">
                   <img src="/icons/folder.png" alt="folder" />
                   <p>{trend.label}</p>
                 </section>
                 <h5 className="text-[#707075]">Posted - {trend.posted}</h5>
               </section>
-              <section className="flex h-[35%] flex-col gap-3">
+              <section className="flex flex-col gap-2 pt-2">
                 <h3 className="text-lg font-bold text-font-black-1">
                   {trend.title}
                 </h3>
                 <p>{trend.description}</p>
-                <Button className="flex w-[35%] items-center justify-evenly bg-bright-2 font-bold text-font-black-3 hover:bg-green-400">
-                  Lebih lengkap <FaArrowRightLong className="text-[#1E212B]" />
-                </Button>
+                <div className="pt-1 md:pt-0">
+                  <Button className="flex items-center justify-center gap-2 bg-bright-2 font-bold text-font-black-3 hover:bg-green-400">
+                    Lebih lengkap
+                    <FaArrowRightLong className="pt-1 text-[#1E212B]" />
+                  </Button>
+                </div>
               </section>
             </section>
           ))}
-          <Pagination className="flex w-full justify-start">
-            <PaginationContent>
-              <PaginationItem>
-                <PaginationPrevious href="#" />
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationLink href="#">1</PaginationLink>
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationEllipsis />
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationNext href="#" />
-              </PaginationItem>
-            </PaginationContent>
-          </Pagination>
         </section>
       </section>
-      <section className="flex h-[15%] w-full items-center">
-        <section className="flex h-1/2 w-full bg-[url('/backgrounds/green.png')]">
-          <section className="container flex w-full">
-            <section className="flex w-1/2 flex-col justify-center gap-3">
-              <h3 className="ml-[5%] w-[80%] text-[1.3rem] font-bold text-font-black-3">
+
+      <section className="flex min-h-[600px] flex-col justify-evenly gap-5 bg-white pt-10">
+        <div className="container h-[1px] w-4/5 bg-[#36373C] md:w-[95%]" />
+        <h1 className="container text-[2rem] font-bold text-font-black-1">
+          All News
+        </h1>
+        <section className="container flex flex-wrap justify-between gap-10">
+          {allNews.map((trend, index) => (
+            <section
+              className="flex min-h-[400px] w-full flex-col justify-between md:w-[29.5%]"
+              key={index}
+            >
+              <section className="flex flex-col pt-1">
+                <img
+                  src={trend.image}
+                  alt="trend"
+                  className=" rounded-md object-cover"
+                />
+                <section className="flex items-center gap-1">
+                  <img src="/icons/folder.png" alt="folder" />
+                  <p>{trend.label}</p>
+                </section>
+                <h5 className="text-[#707075]">Posted - {trend.posted}</h5>
+              </section>
+              <section className="flex flex-col gap-2 pt-2">
+                <h3 className="text-lg font-bold text-font-black-1">
+                  {trend.title}
+                </h3>
+                <p>{trend.description}</p>
+                <div className="pt-1 md:pt-0">
+                  <Button className="flex items-center justify-center gap-2 bg-bright-2 font-bold text-font-black-3 hover:bg-green-400">
+                    Lebih lengkap
+                    <FaArrowRightLong className="pt-1 text-[#1E212B]" />
+                  </Button>
+                </div>
+              </section>
+            </section>
+          ))}
+        </section>
+      </section>
+
+      <section className="flex min-h-[200px] flex-col justify-end gap-5 bg-white pt-20 md:min-h-[400px] md:pt-0 lg:pt-64">
+        <div className="flex h-[250px] bg-[url('/backgrounds/green.png')] bg-cover lg:h-[300px]">
+          <div className="container flex">
+            <div className="flex w-full flex-col justify-center gap-3 md:w-1/2">
+              <h3 className="text-base font-bold text-font-black-3 md:text-lg lg:text-2xl">
                 Dapatkan konsultasi kesehatan yang terpercaya dengan tim ahli
                 medis kami, siap membantu Anda menemukan solusi terbaik untuk
                 kesehatan Anda.
               </h3>
-              <Button className="ml-[5%] w-[25%]">Konsultasi Sekarang</Button>
-            </section>
-
-            <section className="flex w-1/2 items-center justify-center">
+              <div>
+                <Button>Konsultasi Sekarang</Button>
+              </div>
+            </div>
+            <div className="hidden w-1/2 items-center justify-center md:flex">
               <img
                 src="/illustrations/doctor.png"
                 alt="doctor"
-                className="relative -right-24 -top-20 scale-75"
+                className="relative scale-90 md:bottom-[12%] lg:bottom-[21%] xl:bottom-[27.5%]"
               />
-            </section>
-          </section>
-        </section>
+            </div>
+          </div>
+        </div>
       </section>
 
-      {/* Footer */}
-      <Footer />
+      <Footer className="z-10" />
     </main>
   );
 };
