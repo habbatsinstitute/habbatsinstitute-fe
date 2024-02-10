1cf19e37472fdfecbeeb6c4902f577a4fadf5869
RohendoJ <rohendoj3134.github@gmail.com>
2024-02-10 16:41:42 +0700

    improve: loading

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -3,15 +3,17 @@ import { AiOutlineLoading } from "react-icons/ai";
 
 export const Loader: FC = (): ReactElement => {
   return (
-    <main className="container flex h-screen w-screen flex-col items-center justify-center gap-2 bg-emerald-50">
-      <img src="/logos/black.png" alt="logo" className="my-5 w-24" />
-      <div className="flex w-full items-center justify-center gap-3">
-        <p className="text-lg">Mempersiapkan...</p>
-        <AiOutlineLoading className="animate-spin" />
+    <main className="h-screen w-screen bg-emerald-50">
+      <div className="container flex h-full flex-col items-center justify-center gap-2">
+        <img src="/logos/black.png" alt="logo" className="my-5 w-24" />
+        <div className="flex w-full items-center justify-center gap-3">
+          <p className="text-lg">Mempersiapkan...</p>
+          <AiOutlineLoading className="animate-spin" />
+        </div>
+        <p className="text-center">
+          Harap tunggu, kami sedang memproses permintaan anda
+        </p>
       </div>
-      <p className="text-center">
-        Harap tunggu, kami sedang memproses permintaan anda
-      </p>
     </main>
   );
 };
